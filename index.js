e7307e6b80010a04462a3293aa142391aaf4c8fc
Clifford Mapesa <cliffordmapesa8@gmail.com>
2022-08-04 09:00:47 +0300

    Add save function

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 let count = 0;
 
 function increment() {
   
-  count = count + 1;
-  countEl.innerText = count;
+  count += 1;
+  countEl.textContent = count;
   
   
 }
 
 
 
-function save() {
-  console.log(count);
+function save(e) {
+  let saveCount = count + ' - ';
+  saveEl.textContent += saveCount;
+  countEl.textContent = 0;
+  count = 0;
 }
+
+
+
 
